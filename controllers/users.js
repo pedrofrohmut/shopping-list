51d8edafbe62cff524843830bec6d15fc01e33b8
Pedro Frohmut <pedrofrohmut@gmail.com>
2020-07-02 21:57:14 -0300

    Log in user

--- controllers/users.js
+++ controllers/users.js
@@ -50,3 +50,48 @@ exports.registerUser = async (req, res, next) => {
     })
   }
 }
+
+exports.loginUser = async (req, res, next) => {
+  const { email, password } = req.body
+  // Simple validation
+  if (!email || email === "" || !password || password === "") {
+    return res.status(400).json({
+      success: false,
+      message: "Bad Request: The input was not valid and user cant be added"
+    })
+  }
+  try {
+    // Check if existing user
+    const existingUser = await User.findOne({ email })
+    if (!existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "Bad Request: No user found with passed e-mail"
+      })
+    }
+    // Validate user password
+    const passwordsMatch = await bcrypt.compare(password, existingUser.password)
+    if (!passwordsMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Bad Request: Password did not matched for the passed e-mail"
+      })
+    }
+    const token = jwt.sign({ id: existingUser._id }, process.env.JWT_SECRET, {
+      expiresIn: 3600
+    })
+    return res.status(200).json({
+      success: true,
+      data: {
+        user: existingUser,
+        token
+      },
+      message: "Success: User logged in"
+    })
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: "Error: error to log in User: " + err.message
+    })
+  }
+}
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,15 @@
 const router = require("express").Router()
 
-const { registerUser } = require("../../controllers/users")
+const { registerUser, loginUser } = require("../../controllers/users")
 
 // @route POST api/users
-// @desc Register new users
+// @desc Register new user
 // @access Public
 router.route("/").post(registerUser)
+
+// @route POST api/users/login
+// @desc Log in a user
+// @access Public
+router.route("/login").post(loginUser)
 
 module.exports = router
